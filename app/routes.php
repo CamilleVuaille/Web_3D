009cc24f99700053d0d324b411fa6a0a6f39dade
Jibe0123 <Jbagostin@gmail.com>
2016-04-13 23:00:53 +0200

    add some files

--- app/routes.php
+++ app/routes.php
@@ -1,12 +1,26 @@
 <?php
 
+
+$app->register(new Silex\Provider\DoctrineServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+
 // Home page
 $app->get('/', function () {
+    require '../views/header.php';
+    
+    
+    //variable contenant les données necessaire extraite de la base de ddonnée
     require '../src/model.php';
-    $articles = getArticles();
+    
 
     ob_start();             // start buffering HTML output
-    require '../views/view.php';
+    require '../views/home.twig';
     $view = ob_get_clean(); // assign HTML output to $view
     return $view;
+    
+    
+    
 });
