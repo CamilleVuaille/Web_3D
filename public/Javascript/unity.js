9992e0bc68fe04168931552c6cde3a926112aa23
Camille Vuaille <CamilleVuaille@MacBook-Pro-de-Camille-2.local>
2016-04-14 23:16:28 +0200

    maj profile

--- public/Javascript/unity.js
+++ public/Javascript/unity.js
@@ -1,17 +1,23 @@
-//recupere les éléments html 
-var	accueil = document.getElementById("accueil"),
-	connexionInscription = document.getElementById("connexionInscription"),
-	start = document.getElementById("start"),
-	choixLvl = document.getElementById("choixLvl"),
-	aConnexion = document.getElementById("aConnexion");
-	
-//variables globales
-aConnexion.addEventListener("click", function(){
-	connexionInscription.classList.remove('hidden');
-	accueil.classList.add('hidden');
-});
-start.addEventListener("click", function(){
-	choixLvl.classList.remove('hidden');
-	accueil.classList.add('hidden');
-});
+var pen1 = document.getElementById("pen1"),
+	pen2 = document.getElementById("pen2"),
+    profil = document.getElementById("profil");
+
+pen1.onclick = function() {
+  if ( profil.children[0].children[0].tagName != "LABEL") {
+    profil.children[0].children[0].outerHTML = "<label for='profil-edit'><strong>"+profil.children[0].children[0].innerHTML+"</strong></label>";
+    profil.children[0].children[1].outerHTML = "<input id='profil-edit' value='"+profil.children[0].children[1].innerHTML+"'>";
+  } else {
+    profil.children[0].children[0].outerHTML = "<strong>"+profil.children[0].children[0].innerHTML+"</strong>";
+    profil.children[0].children[1].outerHTML = "<span>"+profil.children[0].children[1].value+"</span>";
+  }
+}
+pen2.onclick = function() {
+  if ( profil.children[1].children[0].tagName != "LABEL") {
+     profil.children[1].children[0].outerHTML = "<label for='profil-edit'><strong>"+profil.children[1].children[0].innerHTML+"</strong></label>";
+    profil.children[1].children[1].outerHTML = "<input id='profil-edit' value='"+profil.children[1].children[1].innerHTML+"'>";
+  } else {
+    profil.children[1].children[0].outerHTML = "<strong>"+profil.children[1].children[0].innerHTML+"</strong>";
+    profil.children[1].children[1].outerHTML = "<span>"+profil.children[1].children[1].value+"</span>";
+  }
+}
 
